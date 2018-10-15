907fc4e9b03220d96565cd4a83bc03a99115d7f0
Romain Laï-King <romain@homeloop.fr>
2018-10-15 16:11:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const http = require('http')
+const httpProxy = require('http-proxy')
+const fs = require('fs')
+const jsonFormat = require('json-format')
+const { URL } = require('url');
+const mkdirp = require('mkdirp')
+
+
+const loggedHosts = ['localhost:3030']
+const ignoredMethods = ['OPTIONS']
+
+
+
+const outputFolder = './output/' + new Date().toISOString().substr(0,19)
+mkdirp(outputFolder, err => {
+  if (err)
+    throw err
+})
+
+const jsonConfig = {
+  type: 'space',
+  size: 2
+}
+
+const proxy = httpProxy.createProxyServer({});
+
+let count = 0
+proxy.on('proxyRes', (proxyRes, req, res) => {
+
+  const headers = req.headers
+  const method = proxyRes.req.method
+
+  if (!loggedHosts.includes(headers.host) || ignoredMethods.includes(method)) {
+    console.log('Unlogged: ', method , ' - ' ,req.url)
+    return
+  }
+  const url = new URL(req.url)
+  const trimmedPath = url.pathname.replace(/\//g, '')
+
+  const id = ++count
+
+  function makeFilePath(type, extension) {
+    return `${outputFolder}/${(id + '').padStart(4, 0)}-${type}-${method}-${trimmedPath}.${extension}`
+  }
+     
+  const reqFilePath = makeFilePath('REQ', 'txt')
+  let content = req.url
+  if(headers['content-type'] === 'application/json' && req.data){ 
+    content += jsonFormat(JSON.parse(req.data), jsonConfig)
+  }
+
+  fs.writeFile(reqFilePath, content, err => {
+    if (err){
+      console.error(err)
+    }
+    console.log(id, ' Saved REQUEST:', reqFilePath)
+  })  
+
+
+  proxyRes.on('data',  data => {
+    const text = data.toString('utf8')
+    let content
+    if (proxyRes.headers['content-type'].includes('application/json')) {
+      content = jsonFormat(JSON.parse(text), jsonConfig)
+    } else {
+      content = text
+    }
+    const resFilePath = makeFilePath('RES', 'json')
+    fs.writeFile(resFilePath, content, err => {
+      if (err){
+        console.error(err)
+      }
+      console.log(id, ' Saved RESPONSE:', resFilePath)
+    })
+  })
+})
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+const server = http.createServer( (req, res) => {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.  
+  const url = new URL(req.url)
+  req.on('data', data => {
+    const text = data.toString('utf8')
+    req.data = text
+  })
+
+  console.log('Proxying ', req.url)
+  proxy.web(req, res, { target: url.origin })
+})
+
+console.log("listening on port 5050")
+server.listen(5050)
+
+/*
+127.0.0.1
+::1
+localhost
+*/
